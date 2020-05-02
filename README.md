8cf22b901e5a3be1360724beaa0572f3757eb56b
Sal Belal <sbelal@pnimedia.com>
2020-05-02 13:03:03 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ReactTypeScriptNetCoreSample
+# Sample ReactJS + TypeScript + .Net Core
 A sample app created using ReactJS, TypeScript and .Net Core back-end
+
+
+
+How this project was create:
+dotnet new react -n SampleReactApp
+cd into SampleReactApp 
+Delete ClientApp folder from \SampleReactApp
+npx create-react-app my-app --template typescript
+rename my-app to ClientApp
---
+++ src/SampleReactApp/Controllers/PingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleReactApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        // GET: api/Ping
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Ping/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Ping
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Ping/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
